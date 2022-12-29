9389a01783eb85531eaee8237f969f3e330a6c6c
Musheer Alam <musheeralam360@gmail.com>
2022-12-29 18:19:11 +0530

    Create prime.cpp

---
+++ prime.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, i, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(i = 2; i <= n / 2; i++) {
+        if(n % i == 0) {  
+          cout << n << " is not a prime number." << endl;  
+          count = 1;  
+          break;
+        }
+    }
+
+    if(count == 0) {
+        cout << n << " is a prime number" << endl;
+    }
+
+}
